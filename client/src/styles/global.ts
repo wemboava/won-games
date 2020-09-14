5e9d16419235da350a5155a9f45929927824cf13
willian emboava <w.emboava.o@gmail.com>
2020-09-14 20:37:28 -0300

    :sparkles: Added Poppins font

--- client/src/styles/global.ts
+++ client/src/styles/global.ts
@@ -1,11 +1,43 @@
 import { createGlobalStyle } from 'styled-components'
 
 export default createGlobalStyle`
+  @font-face {
+    font-family: 'Poppins';
+    font-style: normal;
+    font-weight: 300;
+    font-display: swap;
+    src: url('/fonts/poppins-v12-latin-300.eot');
+    src: local('Poppins Light'), local('Poppins-Light'),
+      url('/fonts/poppins-v12-latin-300.woff2') format('woff2'),
+  }
+
+  @font-face {
+    font-family: 'Poppins';
+    font-style: normal;
+    font-weight: 400;
+    font-display: swap;
+    src: url('/fonts/poppins-v12-latin-regular.eot');
+    src: local('Poppins Regular'), local('Poppins-Regular'),
+      url('/fonts/poppins-v12-latin-regular.woff2') format('woff2'),
+  }
+
+  @font-face {
+    font-family: 'Poppins';
+    font-style: normal;
+    font-weight: 600;
+    font-display: swap;
+    src: url('/fonts/poppins-v12-latin-600.eot');
+    src: local('Poppins SemiBold'), local('Poppins-SemiBold'),
+      url('/fonts/poppins-v12-latin-600.woff2') format('woff2'),
+  }
+
   * {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
-    outline: 0
+    outline: 0;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
   }
 
   *:focus {
@@ -17,7 +49,7 @@ export default createGlobalStyle`
   }
 
   body {
-    font-family: --apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
+    font-family: 'Poppins', --apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
     Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
     -webkit-font-smoothing: antialiased !important;
     overflow-x: hidden;
